4469939dedb7ed57870f8b96bc29a52963f85a06
Edgar Goetzendorff <goetzendorff@cip.ifi.lmu.de>
2018-05-22 13:00:20 +0200

    fixes explorer link

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -6,7 +6,7 @@
     <p class="mb-8 text-md">public pool ~ weekly payouts ~ min payout 1 BPL ~ fees covered</p>
 
     <div class="font-normal">
-    <a href="https://bplexp.blockpool.io/address/BHzWuAJbMRLAUKTQfjjn56KR3xoBar6CRi" class="inline-block px-4 py-3 bg-blue text-white shadow">Explorer</a>
+    <a href="http://bplexp.blockpool.io/address/BHzWuAJbMRLAUKTQfjjn56KR3xoBar6CRi" class="inline-block px-4 py-3 bg-blue text-white shadow">Explorer</a>
     <a href="https://github.com/dated/" class="inline-block px-4 py-3 bg-blue text-white shadow">Github</a>
     </div>
   </div>
