44847e7cb549b98844d82248414a7b5b9009ded3
Edgar Goetzendorff <goetzendorff@cip.ifi.lmu.de>
2018-08-28 10:32:58 +0200

    optimizes dashboard for mobile

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="jumbotron max-w-xl bg-grey-lighter mx-auto p-4 xl:p-8 shadow flex flex-col items-end justify-end">
-    <!-- <h1 class="text-5xl my-4"><a href="#">{{ description.title }}</a></h1> -->
+  <div class="jumbotron max-w-xl bg-grey-lighter mx-auto p-4 xl:p-8 shadow flex flex-col items-center sm:items-end justify-end bg-right sm:bg-left">
+    <h1 class="text-5xl my-4 block sm:hidden"><a href="#">{{ description.title }}</a></h1>
 
-    <p v-if="description.desc" class="mb-4 font-normal text-xl text-white">{{ description.desc }}</p>
-    <p v-if="description.subdesc" class="mb-8 text-md text-white">{{ description.subdesc }}</p>
+    <div class="flex flex-col sm:flex-row w-full justify-between items-center">
+      <div class="mb-4 sm:mb-0">
+        <p v-if="description.desc" class="font-normal text-xl text-white" :class="{ 'mb-4': description.subdesc }">{{ description.desc }}</p>
+        <p v-if="description.subdesc" class="mb-8 text-md text-white">{{ description.subdesc }}</p>
+      </div>
 
-    <div class="font-normal">
-      <a v-for="(url, text) in buttons" :key="url" :href="url" class="inline-block px-4 py-3 ml-3 bg-blue text-white shadow">
-        {{ text }}
-      </a>
+      <div class="font-normal w-full sm:w-auto flex text-center">
+        <a v-for="(url, text, index) in buttons" :key="url" :href="url" class="inline-block px-4 py-3 w-full sm:w-auto ml-0 sm:ml-3 bg-blue text-white shadow" :class="{ 'ml-3': index > 0 }">
+          {{ text }}
+        </a>
+      </div>
     </div>
   </div>
 </template>
